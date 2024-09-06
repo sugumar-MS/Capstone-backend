737a32fdecfb1533b827ee5574ea545fe7ebdb77
sugumar-MS <msugumar832@gamil.com>
2024-09-06 22:44:24 +0530

    added README.md

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // index.js
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const cors = require("cors");
 
-// Load environment variables
 dotenv.config();
-
-// Connect to the database
 connectDB();
 
 const app = express();
-
-// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// CORS configuration
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://capstone-frontend-task.netlify.app'
+];
+
 app.use(cors({
-	origin: process.env.ORIGIN || 'http://localhost:5173', // Use the origin from environment variables or default to localhost
-	methods: ["GET", "PUT", "POST", "DELETE"],
-	credentials: true, // Allow credentials like cookies
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 // Routes
@@ -27,6 +32,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
